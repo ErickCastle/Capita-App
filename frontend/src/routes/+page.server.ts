05f739215007f12dfc017483b313e6f28d082c8f
Erick Castillo <me.erickcastle@gmail.com>
2023-04-07 01:10:23 -0400

    Updated HTTP request

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,9 +3,13 @@ import type { Actions } from "./$types";
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
+        const age = data.get("age")
         console.log(data);
 
-        const response = await fetch("http://127.0.0.1:5000/test");
-        console.log(response);
+        const response = await fetch("http://127.0.0.1:5000/test", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(age)
+        });
     }
 } satisfies Actions;
